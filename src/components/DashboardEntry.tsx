16c22e3c73c93d5477e8453837265fe93dcf45f3
ImDanTheDev <dmott248@gmail.com>
2021-06-22 18:45:22 -0400

    Redesigned dashboard UI

--- src/components/DashboardEntry.tsx
+++ src/components/DashboardEntry.tsx
@@ -1,8 +1,9 @@
 import React, { useState, FC, useEffect } from 'react';
 import {
     View,
-    Button,
     TouchableOpacity,
+    StyleSheet,
+    Text,
 } from 'react-native';
 import QRCode from 'react-native-qrcode-svg';
 
@@ -10,26 +11,27 @@ import IQRCodeData from '../IQRCodeData';
 
 interface Props {
     /** The QR code to display. */
-    qrCodeData: IQRCodeData
+    qrCodeData: IQRCodeData;
     /** Callback to delete this QR code. */
     onDelete: (qrCodeData: IQRCodeData) => void;
     /** Callback to print this QR code. */
     onPrint: (base64: string) => Promise<void>;
     /** Calback when the entry is pressed. */
-    onPress: (qrCodeData: IQRCodeData) => Promise<void>
+    onPress: (qrCodeData: IQRCodeData) => Promise<void>;
 }
 
 const DashboardEntry: FC<Props> = (props: Props) => {
     const [svg, setSVG] = useState<any>(undefined);
     const [qrBase64, setQRBase64] = useState<string>('');
+    const [utilityMode, setUtilityMode] = useState<boolean>(false);
 
     // Runs when the generated QR code SVG changes.
     useEffect(() => {
-        if (svg === undefined) return;
+        if (!svg || utilityMode) return;
 
         // Get the base64 representation of the SVG.
         svg.toDataURL(setQRBase64);
-    }, [svg]);
+    }, [svg, utilityMode]);
 
     /** Calls the onPrint callback. */
     const handlePrintQRCode = async () => {
@@ -46,15 +48,82 @@ const DashboardEntry: FC<Props> = (props: Props) => {
         await props.onPress(props.qrCodeData);
     }
 
+    const toggleUtilityMode = () => {
+        setUtilityMode(!utilityMode);
+    }
+
+    if (utilityMode)
+    {
+        return (
+            <View style={styles.qrCodeListEntry}>
+                <TouchableOpacity style={styles.utilityOption} onPress={deleteQRCode}>
+                    <Text style={styles.utilityLabel}>Delete</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.utilityOption} onPress={handlePrintQRCode}>
+                    <Text style={styles.utilityLabel}>Print</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.utilityOption} onPress={toggleUtilityMode}>
+                    <Text style={styles.utilityLabel}>Cancel</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
     return (
-        <View>
-            <TouchableOpacity onPress={handlePress}>
-                <QRCode value={props.qrCodeData.text} getRef={setSVG}/>
-            </TouchableOpacity>
-            <Button onPress={handlePrintQRCode} title='Print' />
-            <Button onPress={deleteQRCode} title='Delete' />
-        </View>
+        <TouchableOpacity style={styles.qrCodeListEntry} onPress={handlePress} onLongPress={toggleUtilityMode}>
+            <View style={styles.detailsSection}>
+                <Text style={{...styles.qrName, fontStyle: props.qrCodeData.name ? 'normal' : 'italic'}}>{props.qrCodeData.name || 'Unnamed'}</Text>
+                <Text style={styles.qrText} numberOfLines={3}>{props.qrCodeData.text}</Text>
+            </View>
+            <View style={styles.qrSection}>
+                <QRCode value={props.qrCodeData.text} size={64} getRef={setSVG}/>
+            </View>
+        </TouchableOpacity>
     );
 }
+
+const styles = StyleSheet.create({
+    qrCodeListEntry: {
+        backgroundColor: '#FFF',
+        borderRadius: 16,
+        marginHorizontal: 16,
+        marginVertical: 8,
+        padding: 16,
+        flexDirection: 'row',
+        flex: 0,
+        height: 96,
+        shadowColor: "#00000088",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.30,
+        shadowRadius: 4.65,
+        elevation: 8,
+        
+    },
+    detailsSection: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between'
+    },
+    qrSection: {
+    },
+    qrName: {
+        color: 'gray',
+        fontWeight: 'bold'
+    },
+    qrText: {
+
+    },
+    utilityOption: {
+        flex: 1,
+        alignItems: 'center',
+        alignSelf: 'center',
+    },
+    utilityLabel: {
+        fontSize: 26
+    }
+});
 
 export default DashboardEntry;
--- src/components/DashboardSceen.tsx
+++ src/components/DashboardSceen.tsx
@@ -2,19 +2,24 @@ import React, { useState, useEffect } from 'react';
 import {
     View,
     FlatList,
-    Button,
     ListRenderItem,
     TextInput,
+    Text,
+    TouchableOpacity,
 } from 'react-native';
 import { Navigation, NavigationFunctionComponent } from 'react-native-navigation';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { ComponentId as CreatorId, Props as CreatorProps} from './CreatorScreen';
 import { ComponentId as PrintPreviewId, Props as PrintPreviewProps} from './PrintPreviewScreen';
-import { ComponentId as ScannerButtonId} from './ScannerButton';
 import { ComponentId as ScannerId, Props as ScannerProps} from './ScannerScreen';
 import IQRCodeData from '../IQRCodeData';
 import DashboardEntry from './DashboardEntry';
+import { StyleSheet } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import Entypo from 'react-native-vector-icons/Entypo';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 interface Props {
     /** react-native-navigation component id. */
@@ -25,27 +30,8 @@ const DashboardScreen: NavigationFunctionComponent<Props> = (props: Props) => {
     // All loaded QR codes in the app.
     const [qrCodes, setQRCodes] = useState<IQRCodeData[]>([]);
     const [searchPhrase, setSearchPhrase] = useState<string>('');
-
     const [newQRCode, setNewQRCode] = useState<IQRCodeData>();
-
-    // Runs once on component mount
-    useEffect(() => {
-        Navigation.mergeOptions(props.componentId, {
-            topBar: {
-                rightButtons: [
-                    {
-                        id: 'openScannerBtn',
-                        component: {
-                            name: ScannerButtonId,
-                            passProps: {
-                                onOpenScanner: handleOpenScanner
-                            }
-                        }
-                    }
-                ]
-            }
-        });
-    }, []);
+    const [searching, setSearching] = useState<boolean>(false);
 
     // Runs once on component mount and when search phrase changes.
     useEffect(() => {
@@ -179,23 +165,212 @@ const DashboardScreen: NavigationFunctionComponent<Props> = (props: Props) => {
         return qrCodes.filter(qrCode => qrCode.name?.startsWith(searchPhrase));
     }
 
+    /** Render groups and sub-headers */
+    const renderListHeader = (): React.ReactElement => {
+        return (
+            <View>
+                <View style={styles.groupsSection}>
+                    <View style={styles.subHeader}>
+                        <Text style={styles.subHeaderTitle}>GROUPS</Text>
+                        <View style={styles.flexFiller}/>
+                        <Text style={styles.subHeaderAction}>View all</Text>
+                    </View>
+                    <View style={styles.groupsList}>
+                        <Text style={styles.noGroupsLabel}>No groups</Text>
+                    </View>
+                </View>
+                <View style={styles.subHeader}>
+                    <Text style={styles.subHeaderTitle}>QR CODES</Text>
+                </View>
+            </View>
+        )
+    }
+
+    /** Add padding to bottom of the QR list. */
+    const renderListFooter = (): React.ReactElement => {
+        return (
+            <View style={{padding: 24}}/>
+        )
+    }
+
+    /** Toggle search bar visibility and clear it when hiding. */
+    const toggleSearchBar = () => {
+        if (searching) {
+            setSearchPhrase('');
+        }
+        setSearching(!searching);
+    }
+
     return (
-        <View>
-            <TextInput onChangeText={setSearchPhrase} />
-            <Button onPress={handleCreateBtn} title='Create'/>
-            <FlatList data={getFilteredQRCodes()} renderItem={renderQRCode} keyExtractor={item => item.id || ''} />
+        <View style={styles.dashboard}>
+            <LinearGradient style={styles.background} colors={['#FFFFFF','#F2F1F6']} >
+                <View style={styles.content}>
+                    <FlatList style={styles.qrCodesList} removeClippedSubviews={false} ListHeaderComponent={renderListHeader} ListFooterComponent={renderListFooter} data={getFilteredQRCodes()} renderItem={renderQRCode} keyExtractor={item => item.id || ''} />
+                    <LinearGradient start={{x: 0, y: 0.75}} end={{x: 0, y: 1}} colors={['#FFFFFFFF','#FFFFFF00']}>
+                        <View style={styles.header}>
+                            {searching ? (
+                                <TextInput style={styles.searchBar} onChangeText={setSearchPhrase}/>
+                            ) : (
+                                <Text style={styles.title}>Your QR Codes</Text>
+                            )}
+                            <TouchableOpacity style={styles.searchBtn} onPress={toggleSearchBar}>
+                                <FontAwesome5 name={searching ? 'times' : 'search'} size={28} color={'gray'}/>
+                            </TouchableOpacity>
+                        </View>
+                    </LinearGradient>
+                </View>
+                <View style={styles.bottomBar}>
+                    <TouchableOpacity style={styles.bottomBarBtn}>
+                        <Entypo name={'grid'} size={48} color={'#3e3e3e'}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.bottomBarPrimaryBtn} onPress={handleCreateBtn}>
+                        <FontAwesome5 name={'plus'} size={38} color={'#FFF'}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.bottomBarBtn} onPress={handleOpenScanner}>
+                        <Ionicons name={'scan'} size={38} color={'#3e3e3e'}/>
+                    </TouchableOpacity>
+                </View>
+            </LinearGradient>
         </View>
     )
 }
 
+const styles = StyleSheet.create({
+    dashboard: {
+        flex: 1,
+        flexDirection: 'column',
+    },
+    background: {
+        flex: 1,
+        flexDirection: 'column'
+    },
+    content: {
+        paddingHorizontal: 16,
+        flexDirection: 'column-reverse',
+        flex: 1
+    },
+    header: {
+        flexShrink: 1,
+        flexDirection: 'row',
+        paddingHorizontal: 16,
+        height: 125,
+        backgroundColor: 'transparent',
+        alignItems: 'center'
+    },
+    title: {
+        color: '#000',
+        flex: 1,
+        alignSelf: 'center',
+        fontSize: 28,
+    },
+    searchBtn: {
+        width: 48,
+        height: 48,
+        borderRadius: 16,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    searchBar: {
+        flex: 1,
+        backgroundColor: '#F2F1F6',
+        borderRadius: 16,
+        color: '#000',
+        paddingHorizontal: 16,
+        fontSize: 16
+    },
+    groupsSection: {
+        flexShrink: 1,
+        minHeight: 96,
+    },
+    groupsList: {
+        flex: 1,
+        flexDirection: 'column',
+    },
+    noGroupsLabel: {
+        flexGrow: 1,
+        alignSelf: 'center',
+        textAlignVertical: 'center',
+        fontSize: 18,
+        color: 'gray',
+    },
+    subHeader: {
+        flexShrink: 1,
+        flexDirection: 'row',
+        margin: 16
+    },
+    subHeaderTitle: {
+        color: 'gray',
+        alignSelf: 'center',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    subHeaderAction: {
+        color: 'gray',
+        alignSelf: 'center',
+        fontSize: 18,
+    },
+    flexFiller: {
+        flex: 1,
+    },
+    qrCodesSection: {
+        flexGrow: 1,
+    },
+    qrCodesList: {
+        flexGrow: 1,
+        overflow: 'visible',
+    },
+    bottomBar: {
+        flexShrink: 0,
+        minHeight: 96,
+        backgroundColor: '#FFF',
+        borderTopLeftRadius: 16,
+        borderTopRightRadius: 16,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 32,
+
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.30,
+        shadowRadius: 4.65,
+        elevation: 8,
+    },
+    bottomBarBtn: {
+        width: 48,
+        height: 48,
+        marginTop: 8,
+        borderRadius: 16,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    bottomBarPrimaryBtn: {
+        backgroundColor: '#5AF0AB',
+        borderRadius: 16,
+        position: 'relative',
+        top: -64/2,
+        width: 64,
+        height: 64,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.20,
+        shadowRadius: 1.41,
+        elevation: 2,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
+
 DashboardScreen.options = {
     topBar: {
-        title: {
-            text: 'Dashboard'
-        }
-    },
+        visible: false
+    }
 };
-
 
 export default DashboardScreen;
 export type { Props };
